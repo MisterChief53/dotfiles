ecd559394bc585463b7bf365859d774a88843934
MisterChief53 <soderol3@hotmail.com>
2023-09-06 08:04:56 -0600

    added tsserver to nvim

--- dot_config/nvim/after/plugin/lsp.lua
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -1,19 +1,20 @@
 local lsp = require('lsp-zero').preset({})
 
 lsp.ensure_installed({
-	-- Replace these with whatever servers you want to install
-	--'tsserver',
-	--'eslint',
-	--'rust_analyzer'
-	'clangd',
-	'cmake',
-	'lua_ls'
+    -- Replace these with whatever servers you want to install
+    --'tsserver',
+    --'eslint',
+    --'rust_analyzer'
+    'clangd',
+    'cmake',
+    'lua_ls',
+    'tsserver'
 })
 
 lsp.on_attach(function(client, bufnr)
-	-- see :help lsp-zero-keybindings
-	-- to learn the available actions
-	lsp.default_keymaps({buffer = bufnr})
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp.default_keymaps({buffer = bufnr})
 end)
 
 -- (Optional) Configure lua language server for neovim

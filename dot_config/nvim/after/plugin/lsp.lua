1b74f6c56ca4b51010ea14bd3058a77a41997285
MisterChief53 <soderol3@hotmail.com>
2023-09-05 19:23:12 -0600

    neovim config files

---
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,22 @@
+local lsp = require('lsp-zero').preset({})
+
+lsp.ensure_installed({
+	-- Replace these with whatever servers you want to install
+	--'tsserver',
+	--'eslint',
+	--'rust_analyzer'
+	'clangd',
+	'cmake',
+	'lua_ls'
+})
+
+lsp.on_attach(function(client, bufnr)
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp.default_keymaps({buffer = bufnr})
+end)
+
+-- (Optional) Configure lua language server for neovim
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+lsp.setup()
---
+++ dot_config/nvim/lua/misterchief53/init.lua
@@ -0,0 +1,3 @@
+require("misterchief53.lazy")
+require("misterchief53.remap")
+require("misterchief53.set")

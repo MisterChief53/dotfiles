c32094474bf45ad85577109bc9b4f2331a0eab82
MisterChief53 <soderol3@hotmail.com>
2023-09-06 18:04:41 -0600

    added emacs init.el

---
+++ dot_config/emacs/init.el
@@ -0,0 +1,39 @@
+;; Add the sources
+(require 'package)
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+(package-initialize)
+
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
+
+;; To automatically update all packages
+(use-package auto-package-update
+  :config
+  (setq auto-package-update-delete-old-versions t)
+  (setq auto-package-update-hide-results t)
+  (auto-package-update-maybe))
+
+;; Company mode for completion
+(use-package company)
+;; Enable company-mode on all buffers
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; Enable line numbers for all buffers
+(global-display-line-numbers-mode t)
+
+
+;; Eglot auto start on mode
+(add-hook 'c++-mode-hook 'eglot-ensure)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(company auto-package-update)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
